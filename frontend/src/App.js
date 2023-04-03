4993d3463f40d6d470bbf0b3d7e91ac8f95447df
trshimpi <trshimpi25@gmail.com>
2023-04-03 16:02:43 +0530

    added product div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">ROYAL</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
